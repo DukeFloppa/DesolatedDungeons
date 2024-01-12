476e00dcdaf5b0a200f48c0d8b5c49396338cb0e
DukeFloppa <65651014+DukeFloppa@users.noreply.github.com>
2024-01-12 16:48:34 +0100

    Initial commit

---
+++ src/main/java/dukefloppa/desolateddungeons/DesolatedDungeons.java
@@ -0,0 +1,16 @@
+package dukefloppa.desolateddungeons;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DesolatedDungeons implements ModInitializer {
+	public static final String MOD_ID = "desolateddungeons";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/dukefloppa/desolateddungeons/DesolatedDungeonsClient.java
@@ -0,0 +1,10 @@
+package dukefloppa.desolateddungeons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DesolatedDungeonsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
