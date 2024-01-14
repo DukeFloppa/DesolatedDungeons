a594db66b54774a7dbcbcf1c05079384924e59c7
DukeFloppa <65651014+DukeFloppa@users.noreply.github.com>
2024-01-14 15:53:44 +0100

    sandsweptRuinsAdvancement structure criterion updated to use new structure, added 2 missing block item models

--- src/main/java/dukefloppa/desolateddungeons/datagen/ModAdvancementProvider.java
+++ src/main/java/dukefloppa/desolateddungeons/datagen/ModAdvancementProvider.java
@@ -12,9 +12,10 @@ import net.minecraft.advancement.criterion.InventoryChangedCriterion;
 import net.minecraft.advancement.criterion.TickCriterion;
 import net.minecraft.item.Items;
 import net.minecraft.predicate.entity.LocationPredicate;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.world.gen.structure.StructureKeys;
 
 import java.util.function.Consumer;
 
@@ -27,18 +28,17 @@ public class ModAdvancementProvider extends FabricAdvancementProvider {
     public void generateAdvancement(Consumer<AdvancementEntry> consumer) {
         AdvancementEntry rootAdvancement = Advancement.Builder.create()
                 .display(
-                        ModBlocks.SALMON_WOOL, // The display icon
-                        Text.translatable("advancements.desolated_dungeons.root.title"), // The title
-                        Text.translatable("advancements.desolated_dungeons.root.description"), // The description
-                        new Identifier("textures/block/sand.png"), // Background image used
-                        AdvancementFrame.TASK, // Options: TASK, CHALLENGE, GOAL
-                        false, // Show toast top right
-                        false, // Announce to chat
-                        false // Hidden in the advancement tab
+                        ModBlocks.SALMON_WOOL,
+                        Text.translatable("advancements.desolated_dungeons.root.title"),
+                        Text.translatable("advancements.desolated_dungeons.root.description"),
+                        new Identifier("textures/block/sand.png"),
+                        AdvancementFrame.TASK,
+                        false,
+                        false,
+                        false
                 )
                 // The first string used in criterion is the name referenced by other advancements when they want to have 'requirements'
                 .criterion("root", InventoryChangedCriterion.Conditions.items(Items.CRAFTING_TABLE))
-                //.criterion("found_sandswept_ruins", TickCriterion.Conditions.createLocation(new LocationPredicate.Builder().structure(StructureKeys.DESERT_PYRAMID)))
                 .build(consumer, DesolatedDungeons.MOD_ID + "/root");
 
         AdvancementEntry sandsweptRuinsAdvancement = Advancement.Builder.create().parent(rootAdvancement)
@@ -52,7 +52,8 @@ public class ModAdvancementProvider extends FabricAdvancementProvider {
                         true,
                         false
                 )
-                .criterion("found_sandswept_ruins", TickCriterion.Conditions.createLocation(new LocationPredicate.Builder().structure(StructureKeys.DESERT_PYRAMID)))
+                .criterion("found_sandswept_ruins", TickCriterion.Conditions
+                        .createLocation(new LocationPredicate.Builder().structure(RegistryKey.of(RegistryKeys.STRUCTURE, new Identifier("desolated_dungeons:sandswept_ruins")))))
                 .build(consumer, DesolatedDungeons.MOD_ID + "/sandswept_ruins");
     }
 }
